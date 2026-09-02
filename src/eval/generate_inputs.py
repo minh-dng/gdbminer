6ab5103381c62380a7a76cd198ddbe9c1347122a
Steven Dang <dang.steven13@gmail.com>
2026-09-03 03:50:48 +1000

    fix: restore trial directory selection

--- src/eval/generate_inputs.py
+++ src/eval/generate_inputs.py
@@ -19,10 +19,12 @@ PRECISION_SIZE = 1000
 def find_output_directory(output_directory_base: Path) -> Path:
     # Find last 'trial-*' folder
     return next(
-        sorted(
-            output_directory_base.glob("trial-*"),
-            key=lambda x: int(x.name.split("-")[1]),
-            reverse=True,
+        iter(
+            sorted(
+                output_directory_base.glob("trial-*"),
+                key=lambda x: int(x.name.split("-")[1]),
+                reverse=True,
+            )
         )
     )
 
