ad7ad419ef43654f89c2d2f3d709f38280b5fc82
Steven Dang <dang.steven13@gmail.com>
2026-09-06 21:41:40 +1000

    refactor: group duplicated find_output_directory in util module

---
+++ src/util/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+
+def find_output_directory(output_directory_base: Path) -> Path:
+    """Find the last 'trial-*' folder in the output directory base."""
+    return next(
+        sorted(
+            output_directory_base.glob("trial-*"),
+            key=lambda x: int(x.name.split("-")[1]),
+            reverse=True,
+        )
+    )
